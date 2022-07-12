189111c800ebc897f38f4403a842867047ec8942
laurenHorton <laurenelizabethhorton@gmail.com>
2022-07-12 21:53:40 +0100

    feat(Map): add dynamic markers on map

--- src/Map.jsx
+++ src/Map.jsx
@@ -1,20 +1,39 @@
 import React from 'react';
-import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet'
-import './App.css'
-const position = [51.505, -0.09]
+import { MapContainer, TileLayer, Marker } from 'react-leaflet';
+import './App.css';
 
-const Map = () => (
-  <MapContainer style={{height: '400px'}} center={position} zoom={13} scrollWheelZoom={false}>
+const getBounds = (articleData) => {
+  let lowestLat = articleData[0].location.latitude;
+  let highestLat = articleData[0].location.latitude;
+  let lowestLong = articleData[0].location.longitude;
+  let highestLong =  articleData[0].location.longitude;
+  articleData.forEach((article) => {
+    const { latitude, longitude } = article.location;
+    if( latitude < lowestLat) lowestLat = latitude;
+    if( latitude > highestLat) highestLat = latitude;
+    if( longitude < lowestLong) lowestLong = longitude;
+    if( longitude > highestLong) highestLong = longitude;
+  });
+
+  return [
+    [lowestLat, lowestLong],
+    [highestLat, highestLong]
+  ]
+}
+
+const Map = ({articleData}) => (
+  <MapContainer style={{height: '400px'}} bounds={getBounds(articleData)} scrollWheelZoom={false}>
     <TileLayer
       attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
       url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
     />
-    <Marker position={[51.505, -0.09]}>
-      <Popup>
-        A pretty CSS3 popup. <br /> Easily customizable.        
-      </Popup>
-    </Marker>
+    {
+        articleData.map((article) => (
+          <Marker position={[article.location.latitude, article.location.longitude]}>
+          </Marker>
+        ))
+    }
   </MapContainer>
-)
+);
 
 export default Map;
